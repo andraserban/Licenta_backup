3143e545d6f5e0ffedf32da2c33f0c9c223810ec
andraserban <andra.serban2095@gmail.com>
2018-03-15 22:23:19 +0200

    login first tryout

---
+++ rssMergedWithArduino/rssMergedWithArduino/LogInForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace rssMergedWithArduino
+{
+    partial class LogInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(76, 67);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(100, 20);
+            this.txt_UserName.TabIndex = 0;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(76, 107);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 20);
+            this.txt_Password.TabIndex = 1;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(76, 161);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Submit.TabIndex = 2;
+            this.btn_Submit.Text = "LogIn";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            // 
+            // LogInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_UserName);
+            this.Name = "LogInForm";
+            this.Text = "LogInForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_Submit;
+    }
+}
---
+++ rssMergedWithArduino/rssMergedWithArduino/LogInForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rssMergedWithArduino
+{
+    public partial class LogInForm : Form
+    {
+        public LogInForm()
+        {
+            InitializeComponent();
+        }
+
+        //Connection String
+        //  string cs = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\MyDatabase.mdf;Integrated Security=True;";
+
+        string cs = @"Data Source=ANDRA;" + "Initial Catalog=AuthenticationCodeFirst;" + "Integrated Security=SSPI;" + "providerName = System.Data.SqlCLient";
+        //btn_Submit Click event
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if (txt_UserName.Text == "" || txt_Password.Text == "")
+            {
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            try
+            {
+                //Create SqlConnection
+                SqlConnection con = new SqlConnection(cs);
+                SqlCommand cmd = new SqlCommand("Select * from tbl_LogInDatabases where UserName=@username and Password=@password", con);
+                cmd.Parameters.AddWithValue("@username", txt_UserName.Text);
+                cmd.Parameters.AddWithValue("@password", txt_Password.Text);
+                con.Open();
+                SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapt.Fill(ds);
+                con.Close();
+                int count = ds.Tables[0].Rows.Count;
+                
+                if (count == 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    this.Hide();
+                    Form1 form = new Form1();
+                    form.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
